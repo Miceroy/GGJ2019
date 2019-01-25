6646ad0178080ce5ba954199b7bf878dfa23933d
Mikko Romppainen <kajakbros@gmail.com>
2019-01-26 00:59:12 +0200

    Lisätty tageja. Muutettu player prefab ja piece prefab.

--- GGJ2019/Assets/Scripts/PieceObject.cs
+++ GGJ2019/Assets/Scripts/PieceObject.cs
@@ -8,14 +8,65 @@ public class PieceObject : GameBase {
     public GameObject outAreaMesh;
 
     bool isInArea;
+    bool isPrevDay;
 
     // Use this for initialization
     void Start () {
-		
-	}
+        checkSwitchMesh();
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
+        bool isNowDay = gameController().isDay();
+        if (isPrevDay != isNowDay)
+        {
+            checkSwitchMesh();
+        }
+        isPrevDay = isNowDay;
 	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Area")
+        {
+            Debug.Log("Piece enter to area");
+            isInArea = true;
+            checkSwitchMesh();
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Area")
+        {
+            Debug.Log("Piece enter to area");
+            isInArea = false;
+            checkSwitchMesh();
+        }
+    }
+
+    private void checkSwitchMesh()
+    {
+        if( gameController().isDay() )
+        {
+            dayMesh.SetActive(true);
+            inAreaMesh.SetActive(false);
+            outAreaMesh.SetActive(false);
+        }
+        else
+        {
+            if(isInArea)
+            {
+                dayMesh.SetActive(false);
+                inAreaMesh.SetActive(true);
+                outAreaMesh.SetActive(false);
+            }
+            else
+            {
+                dayMesh.SetActive(false);
+                inAreaMesh.SetActive(false);
+                outAreaMesh.SetActive(true);
+            }
+        }
+    }
 }
