4f3c5fd5cbc39927aba33b6882ebaff4483417ef
Mikko Romppainen <kajakbros@gmail.com>
2019-01-26 01:44:11 +0200

    Grabbi ja release pelittää itemeille.

--- GGJ2019/Assets/Scripts/PlayerCharacterController.cs
+++ GGJ2019/Assets/Scripts/PlayerCharacterController.cs
@@ -4,16 +4,39 @@ using UnityEngine;
 
 public class PlayerCharacterController : GameBase
 {
+    public GameObject objectPickPoint;
+
+    GameObject collidingItem;
+    Transform collidingOldParent;
+    bool picked;
+
     // Start is called before the first frame update
     void Start()
     {
-
+        collidingItem = null;
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        if (collidingItem && Input.GetButtonUp("Fire1"))
+        {
+            if (picked)
+            {
+                //TODO: Release
+                Debug.Log("Player release");
+                picked = false;
+                collidingItem.transform.SetParent(collidingOldParent);
+            }
+            else
+            {
+                // Grab
+                Debug.Log("Player grab");
+                picked = true;
+                collidingOldParent = collidingItem.transform.parent;
+                collidingItem.transform.SetParent(objectPickPoint.transform);                
+            }
+        }
     }
 
     private void OnTriggerEnter(Collider other)
@@ -21,20 +44,38 @@ public class PlayerCharacterController : GameBase
         if (other.gameObject.tag == "Piece")
         {
             Debug.Log("Collide enter to piece");
-            highlight(true, other.gameObject);
+            //  highlight(true, other.gameObject);
+            collidingItem = other.gameObject;
         }
     }
 
     private void OnTriggerExit(Collider other)
     {
-        if (other.gameObject.tag == "Piece")
+        if (!picked && other.gameObject.tag == "Piece")
         {
             Debug.Log("Collide leave to piece");
-            highlight(false, other.gameObject);
+            //  highlight(false, other.gameObject);
+           // if (!picked)
+           // {
+            //    Debug.Log("Player collidingItem==null");
+                collidingItem = null;
+        //    }
         }
     }
 
-    private void highlight(bool isOn, GameObject go)
+   /* private void highlight(bool isOn, GameObject go)
     {
-    }
+        if (isOn)
+        {
+            collidingItem = go;
+        }
+        else
+        {
+            if (!picked)
+            {
+                Debug.Log("Player collidingItem==null");
+                collidingItem = null;
+            }
+        }
+    }*/
 }
