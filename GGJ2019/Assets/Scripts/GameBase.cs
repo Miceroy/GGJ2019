265f841e6f7fb74032c798be5d067f29fdc23c6f
Janzku <jani.moil@gmail.com>
2019-01-26 14:26:46 +0200

    - Tweaked Shaders a bit.
    - Implemented Lit shaders with dissolve or hologram sub shader.
    - Tweaked GameController to act as a GameBase object manager.
    - Tweaked GameBase object to support GameController day/night cycle.
    - Tweaked PieceObject to neatly dissolve out and in it's other forms.

--- GGJ2019/Assets/Scripts/GameBase.cs
+++ GGJ2019/Assets/Scripts/GameBase.cs
@@ -2,13 +2,48 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameBase : MonoBehaviour {
-    GameController controller;
+public class GameBase : MonoBehaviour
+{
+    [SerializeField]
+    protected string m_id = string.Empty;
+    public string ID { get { return m_id; } }
 
-    public GameController gameController() {
-        if(!controller) {
-            controller = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameController>();
-        }
-        return controller;
+    [SerializeField]
+    protected bool m_enableOnStart = false;
+
+    [SerializeField]
+    protected Material m_hologramBaseMaterial = null;
+
+    [SerializeField]
+    protected Material m_dissolveBaseMaterial = null;
+
+    protected string m_dissolveAmount = "Vector1_EFB0DCB7";
+
+    private void Start()
+    {
+        if (m_enableOnStart)
+            gameObject.SetActive(true);
+    }
+
+    private void OnEnable()
+    {
+        GameController.AddGameBaseObject(this);
+    }
+
+    private void OnDisable()
+    {
+        GameController.RemoveGameBaseObject(this);
+    }
+
+    protected virtual void Update()
+    {
+    }
+
+    public virtual void SwitchToDay()
+    {
+    }
+
+    public virtual void SwitchToNight()
+    {
     }
 }
