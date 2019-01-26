1300ec40d8fdd7828f63af9d6c83d60a3463c805
Mikko Romppainen <kajakbros@gmail.com>
2019-01-26 14:40:42 +0200

    up

--- GGJ2019/Assets/Scripts/PlayerCharacterController.cs
+++ GGJ2019/Assets/Scripts/PlayerCharacterController.cs
@@ -8,7 +8,7 @@ public class PlayerCharacterController : GameBase
 
     GameObject collidingItem;
     Transform collidingOldParent;
-    float oldY;
+    //float oldY;
     bool picked;
 
     // Start is called before the first frame update
@@ -28,10 +28,10 @@ public class PlayerCharacterController : GameBase
                 Debug.Log("Player release");
                 picked = false;
                 collidingItem.transform.SetParent(collidingOldParent);
-                collidingItem.transform.position.Set(
+                /*collidingItem.transform.position.Set(
                     collidingItem.transform.position.x, 
                     oldY, 
-                    collidingItem.transform.position.z);
+                    collidingItem.transform.position.z);*/
             }
             else
             {
@@ -40,7 +40,11 @@ public class PlayerCharacterController : GameBase
                 picked = true;
                 collidingOldParent = collidingItem.transform.parent;
                 collidingItem.transform.SetParent(objectPickPoint.transform);
-                oldY = objectPickPoint.transform.position.y;
+                //oldY = objectPickPoint.transform.position.y;
+               /* collidingItem.transform.position.Set(
+                    0,
+                    objectPickPoint.transform.localPosition.y,
+                    0);*/
             }
         }
     }
