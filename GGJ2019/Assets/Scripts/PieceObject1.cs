f38e33e2b59cbca0b0e7ac1ec0a40c0cebb84416
Mikko Romppainen <kajakbros@gmail.com>
2019-01-27 05:21:29 +0200

    fdfgdhrtyry46556456456464

--- GGJ2019/Assets/Scripts/PieceObject1.cs
+++ GGJ2019/Assets/Scripts/PieceObject1.cs
@@ -151,7 +151,6 @@ public class PieceObject1 : GameBase
         NightObjectOut.GetComponent<MeshRenderer>().material.SetInt(m_hologramToggle, 0);
     }
 
-
     protected override void Update()
     {
         if (m_inTransition)
